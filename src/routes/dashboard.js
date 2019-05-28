87a5f92d21337e0bb71d25785aadf8c44f9df6d3
Melinna <melin1504@gmail.com>
2019-05-27 23:15:49 -0500

    Being creative

--- src/routes/dashboard.js
+++ src/routes/dashboard.js
@@ -57,7 +57,65 @@ router.get('/boleta', async (req, res) => {
 
 //Ahora que ya está elijo todo lo demas
 //Añadir un producto
+//Se supone que el usuario hace clic en agregar aparece el modal y esto sucede
+  //leo el nombre
+/*  const{
+    pro_nombre
+  } = req.body;
 
+  //lo guardo en esta constante
+  const producto ={
+    pro_nombre
+  }
+
+  //busco el id del producto para enviarlo ami proxima queries, asi como precio y medida
+  const resultpro = await pool.query('SELECT pro_id pro_precio,pro_umed FROM producto WHERE pro_nombre = ?', [producto]);
+
+  //se supone que tengo que enviarlo a los campos uwuwuwuw
+  //espacio para enviar xdxdxd
+  //leo el precio y la cantidad
+  const{
+    pro_precio,
+    detb_cantidad
+  } = req.body;
+
+  const monto ={
+    pro_nombre,
+    detb_cantidad
+  }
+
+
+  //ahora tengo que hallar el monto totoal uwuwuwuw
+  const detb_precio = monto.pro_precio*monto.detb_cantidad;
+
+
+  // se supone que envio el monto total
+  router.get('/', async(req,res)=>{
+  res.render('dashboard/boleta', {detb_precio});
+
+  //leo los valores de todos los input
+  const {
+      detb_cantidad,
+      pro_umed,
+      pro_nombre,
+      pro_precio,
+      detb_precio
+    } = req.body;
+  //creo lo que enviaré a mi db
+
+const detalle = {
+    detb_cantidad,
+    pro_umed,
+    pro_nombre,
+    pro_precio,
+    detb_precio
+  };
+
+  // Enviar la query
+  //al parecer tengo que enviar queries a diferentes tablas :((( xdxdxd)))
+
+  await pool.query('INSERT INTO detalle_boleta set ?',[detalle]);
+*/
 res.render('dashboard/boleta');
 });
 
